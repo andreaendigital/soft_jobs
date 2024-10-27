948f9796a4867ce7a42239b911418b78f4fb2cb6
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-10-27 17:41:48 -0300

    ruta post login y usuarios

---
+++ back/index.js
@@ -0,0 +1,38 @@
+const express = require("express"); // importamos express
+const cors = require("cors"); // importamos cors
+const app = express(); //Creamos una instancia de express
+
+const {  getJobs, verificarCredenciales, agregarUsuario } = require("./consultas");
+
+app.listen(3000, () => console.log("Servidor arriba en el puerto 3000 ;)"));
+
+app.use(express.json()); //middleware
+
+app.use(cors()); //CORS
+
+const JWT_SECRET_KEY = "clave_secreta_soft_jobs";
+
+app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      await verificarCredenciales(email, password);
+  
+      const token = jwt.sign({ email }, jwt_secret_key);
+      res.json({ token });
+    } catch (error) {
+      console.log("Error en /login:", error);
+      res.status(error.code || 500).send(error.message);
+    }
+  });
+  
+  app.post("/usuarios", async (req, res) => {
+    try {
+      await agregarUser(req.body);
+      res.status(201).send("Usuario agregado con exito ;)");
+    } catch (error) {
+      res
+        .status(error.code || 500)
+        .send(error.message || "Error al crear al usuario");
+    }
+  });
+
